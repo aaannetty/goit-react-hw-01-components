e4f727921fa9b4185ce58de79add5b76ce6ffecb
aaannetty <latsdoit@gmail.com>
2023-07-08 00:12:34 +0300

    User created

--- src/index.js
+++ src/index.js
@@ -2,9 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import user from './components/user.json';
+
+const Profile = ({ username, tag, location, avatar, stats }) => (
+  <div className="profile">
+    <div className="description">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+        alt={avatar}
+        className="avatar"
+      />
+      <p className="name">{username}</p>
+      <p className="tag">{tag}</p>
+      <p className="location">{location}</p>
+    </div>
+
+    <ul className="stats">
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">1000</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">2000</span>
+      </li>
+      <li>
+        <span className="label">Likes</span>
+        <span className="quantity">3000</span>
+      </li>
+    </ul>
+  </div>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Profile />
   </React.StrictMode>
 );
